75cc507059af61167bed704b9d1c3b7958ade07f
Alex Koumparos <crossroadsman@gmail.com>
2018-04-03 09:43:14 -0600

    Create ciphers.py

---
+++ ciphers.py
@@ -0,0 +1,28 @@
+def rail_fence(plaintext, num_rails=3):
+    '''Description of cipher goes here
+    '''
+    
+    rails = []
+    
+    # Initialise rails
+    for i in range(num_rails):
+        rail = []
+        for j in range(len(plaintext)):
+            rail.append("")
+        rails.append(rail)
+
+    # Encode plaintext
+    rail_number = 0
+    for i in range(len(plaintext)):
+        rails[rail_number][i] = plaintext[i]
+        rail_number = (rail_number + 1) % num_rails
+        
+    # Flatten text
+    # TBD
+    
+    # Group characters
+    # TBD
+        
+    return rails
+    
+    
