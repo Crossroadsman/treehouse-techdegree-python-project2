36df93e9f1d45ecbf51d755ed67ba1351dd22182
Alex Koumparos <crossroadsman@gmail.com>
2018-04-03 10:05:23 -0600

    Update ciphers.py

--- ciphers.py
+++ ciphers.py
@@ -1,9 +1,17 @@
-def rail_fence(plaintext, num_rails=3):
+def rail_fence(plaintext, num_rails=3, strip_punctuation=True):
     '''Description of cipher goes here
     '''
     
+    STRIPPED_CHARACTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                           '0','1','2','3','4','5','6','7','8','9']
+    
     rails = []
     
+    # Optionally strip punctuation
+    if strip_punctuation:
+        plaintext = [c for c in plaintext if c in STRIPPED_CHARACTERS]
+
+        
     # Initialise rails
     for i in range(num_rails):
         rail = []
