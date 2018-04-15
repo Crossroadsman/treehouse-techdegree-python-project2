3b82ca92c2d3a8af385a414f64b2246edd5f2b11
CrossroadsMan <crossroadsman@gmail.com>
2018-04-15 01:00:05 -0600

    Remove redundant file

--- helpers.py
+++
@@ -1,9 +0,0 @@
-def flatten_list(nested):
-    '''takes a list with an inner iterable and flattens it to a single list
-    '''
-    output_list = []
-    for inner in nested:
-        for element in inner:
-            output_list.append(element)
-    return output_list
-
