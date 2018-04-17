f1187a3c07e16804f425b4eee47849ee2d0cc397
CrossroadsMan <crossroadsman@gmail.com>
2018-04-17 10:56:16 -0600

    Create full test suite for Caesar

--- caesar.py
+++ caesar.py
@@ -4,20 +4,21 @@ from ciphers import Cipher
 
 
 class Caesar(Cipher):
-    FORWARD = string.ascii_uppercase * 3
+    UPPERCASE = string.ascii_uppercase
+    FORWARD = UPPERCASE * 3
 
     def __init__(self, offset=3, grouping=5):
         self.offset = offset
-        self.FORWARD = string.ascii_uppercase + string.ascii_uppercase[:self.offset+1]
-        self.BACKWARD = string.ascii_uppercase[:self.offset+1] + string.ascii_uppercase
+
+        self.FORWARD = self.UPPERCASE + self.UPPERCASE[:self.offset+1]
+        self.BACKWARD = self.UPPERCASE[:self.offset+1] + self.UPPERCASE
         self.grouping = grouping
         if self.grouping != 0:
             self.PASSTHROUGH_CHARACTERS = []
 
     def encrypt(self, text):
         # reduce plaintext to valid characters
-        if self.grouping != 0:
-            text = self._reduce_characters(text)
+        text = self._reduce_characters(text)
 
         output = []
         text = text.upper()
@@ -45,3 +46,43 @@ class Caesar(Cipher):
             else:
                 output.append(self.BACKWARD[index-self.offset])
         return ''.join(output)
+
+# -------------------------------------------------------------
+
+if __name__ == "__main__":
+
+    def run_tests(cipher_class, plaintext, tests):
+        for key, value in tests.items():
+            print('\ntest {}'.format(key))
+            kwargs = value
+            cipher = cipher_class(**kwargs)
+            print("encrypting {}:".format(plaintext))
+            encrypted = cipher.encrypt(plaintext)
+            print(encrypted)
+            print("decrypting {}:".format(encrypted))
+            decrypted = cipher.decrypt(encrypted)
+            print(decrypted)
+
+
+    print("Run Test Suite")
+    print("==============")
+    tests = {
+        'a: defaults': {},
+        'b: offset only (4)': {'offset': 4},
+        'c: grouping only (none)': {'grouping': 0},
+        'd: grouping only (3)': {'grouping': 3},
+        'e: offset (4) and grouping (3)': {'offset': 4,
+                                           'grouping': 3}
+    }
+    
+    test_sets = [
+        'the quick brown fox jumps over the lazy dog',
+        'numb3r5 and punctuat!0n',
+        'Hello Peers'
+    ]
+
+    for i in range(len(test_sets)):
+        print("\nTest set {}:".format(i + 1))
+        print("-----------")
+        plaintext = test_sets[i]
+        run_tests(Caesar, plaintext, tests)
